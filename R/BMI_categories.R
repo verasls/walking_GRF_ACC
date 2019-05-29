02f0765fc3afa251d400d1824132a5065186a813
Lucas Veras <lucasdsveras@gmail.com>
2019-05-29 17:13:06 +0100

    Create 1st version of BMI_categeries function

---
+++ R/BMI_categories.R
@@ -0,0 +1,30 @@
+BMI_categories <- function(df) {
+  
+  df$BMI_cat <- NA
+  
+  for (i in 1:nrow(df)) {
+    if (df$BMI[i] >= 18.5 & df$BMI[i] < 25) {
+      df$BMI_cat[i] <- "normal weight"
+    } else {
+      if (df$BMI[i] >= 25 & df$BMI[i] < 30) {
+        df$BMI_cat[i] <- "overweight"
+      } else {
+        if (df$BMI[i] >= 30 & df$BMI[i] < 35) {
+          df$BMI_cat[i] <- "class I obesity"
+        } else {
+          if (df$BMI[i] >= 35 & df$BMI[i] < 40) {
+            df$BMI_cat[i] <- "class II obesity"
+          } else {
+            if (df$BMI[i] >= 40) {
+              df$BMI_cat[i] <- "class III obesity"
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  df$BMI_cat <- factor(df$BMI_cat, levels = c("normal weight", "overweight", "class I obesity", "class II obesity", "class III obesity"))
+  
+  return(df)
+}
