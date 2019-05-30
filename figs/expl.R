780ac505b07c04e49d1109d85e652b15f7dcf35e
Lucas Veras <lucasdsveras@gmail.com>
2019-05-30 12:21:11 +0100

    Revert plot axes

--- figs/expl.R
+++ figs/expl.R
@@ -32,57 +32,57 @@ LOOCV_hip_res    <- BMI_categories(LOOCV_hip_res)
 
 # Ankle
 ankle_pVGRF_pVACC__plot <- ggplot(data = LOOCV_ankle_vert) +
-  geom_point(mapping = aes(x = pVGRF_N, y = pVACC_g, shape = BMI_cat))
+  geom_point(mapping = aes(x = pVACC_g, y = pVGRF_N, shape = BMI_cat))
 
 # Back
 back_pVGRF_pVACC_plot <- ggplot(data = LOOCV_back_vert) +
-  geom_point(mapping = aes(x = pVGRF_N, y = pVACC_g, shape = BMI_cat))
+  geom_point(mapping = aes(x = pVACC_g, y = pVGRF_N, shape = BMI_cat))
 
 # Hip
 hip_pVGRF_pVACC_plot <- ggplot(data = LOOCV_hip_vert) +
-  geom_point(mapping = aes(x = pVGRF_N, y = pVACC_g, shape = BMI_cat))
+  geom_point(mapping = aes(x = pVACC_g, y = pVGRF_N, shape = BMI_cat))
 
 # Actual pRGRF vs pRACC ---------------------------------------------------
 
 # Ankle
 ankle_pRGRF_pRACC_plot <- ggplot(data = LOOCV_ankle_res) +
-  geom_point(mapping = aes(x = pRGRF_N, y = pRACC_g, shape = BMI_cat))
+  geom_point(mapping = aes(x = pRACC_g, y = pRGRF_N, shape = BMI_cat))
 
 # Back
 back_pRGRF_pRACC_plot <- ggplot(data = LOOCV_back_res) +
-  geom_point(mapping = aes(x = pRGRF_N, y = pRACC_g, shape = BMI_cat))
+  geom_point(mapping = aes(x = pRACC_g, y = pRGRF_N, shape = BMI_cat))
 
 # Hip
 hip_pRGRF_pRACC_plot <- ggplot(data = LOOCV_hip_res) +
-  geom_point(mapping = aes(x = pRGRF_N, y = pRACC_g, shape = BMI_cat))
+  geom_point(mapping = aes(x = pRACC_g, y = pRGRF_N, shape = BMI_cat))
 
 # Predicted pVGRF vs pVACC ---------------------------------------------------
 
 # Ankle
 ankle_pred_pVGRF_pVACC_plot <- ggplot(data = LOOCV_ankle_vert) +
-  geom_point(mapping = aes(x = pVGRF_N_predicted, y = pVACC_g, shape = BMI_cat))
+  geom_point(mapping = aes(x = pVACC_g, y = pVGRF_N_predicted, shape = BMI_cat))
 
 # Back
 back_pred_pVGRF_pVACC_plot <- ggplot(data = LOOCV_back_vert) +
-  geom_point(mapping = aes(x = pVGRF_N_predicted, y = pVACC_g, shape = BMI_cat))
+  geom_point(mapping = aes(x = pVACC_g, y = pVGRF_N_predicted, shape = BMI_cat))
 
 # Hip
 hip_pred_pVGRF_pVACC_plot <- ggplot(data = LOOCV_hip_vert) +
-  geom_point(mapping = aes(x = pVGRF_N_predicted, y = pVACC_g, shape = BMI_cat))
+  geom_point(mapping = aes(x = pVACC_g, y = pVGRF_N_predicted, shape = BMI_cat))
 
 # Predicted pRGRF vs pRACC ---------------------------------------------------
 
 # Ankle
 ankle_pred_pRGRF_pRACC_plot <- ggplot(data = LOOCV_ankle_res) +
-  geom_point(mapping = aes(x = pRGRF_N_predicted, y = pRACC_g, shape = BMI_cat))
+  geom_point(mapping = aes(x = pRACC_g, y = pRGRF_N_predicted, shape = BMI_cat))
 
 # Back
 back_pred_pRGRF_pRACC_plot <- ggplot(data = LOOCV_back_res) +
-  geom_point(mapping = aes(x = pRGRF_N_predicted, y = pRACC_g, shape = BMI_cat))
+  geom_point(mapping = aes(x = pRACC_g, y = pRGRF_N_predicted, shape = BMI_cat))
 
 # Hip
 hip_pred_pRGRF_pRACC_plot <- ggplot(data = LOOCV_hip_res) +
-  geom_point(mapping = aes(x = pRGRF_N_predicted, y = pRACC_g, shape = BMI_cat))
+  geom_point(mapping = aes(x = pRACC_g, y = pRGRF_N_predicted, shape = BMI_cat))
 
 # Actual vs Predicted pGRF ------------------------------------------------
 
