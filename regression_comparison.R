30b7f27e2e3c2f6bd93a343ba399714aa05fa826
Lucas Veras <lucasdsveras@gmail.com>
2019-06-03 21:50:18 +0100

    Change inclusion criteria from >= 35 to >= 30 kg/m2 and add non-obese comparison

--- regression_comparison.R
+++ regression_comparison.R
@@ -2,26 +2,44 @@ source("analysis.R")
 
 # Prepare data frame ------------------------------------------------------
 
-hip_vert_df <- LOOCV_hip_vert %>% 
+obese_data <- LOOCV_hip_vert_LMM %>% 
   select(ID, speed, body_mass, BMI, pVACC_g, pVGRF_N, pVGRF_N_predicted) %>% 
-  filter(BMI >= 35) # filters for class II-II obesity
+  filter(BMI >= 30) # filters for class II-II obesity
+
+non_obese_data <- LOOCV_hip_vert_LMM %>% 
+  select(ID, speed, body_mass, BMI, pVACC_g, pVGRF_N, pVGRF_N_predicted) %>% 
+  filter(BMI < 30) # filters for normal weight and overweight
 
 # Apply Neugebauer 2014 equation ------------------------------------------
 
-hip_vert_df$pVGRF_N_Neugebauer <- NA
-for (i in 1:nrow(hip_vert_df)) {
-  hip_vert_df$pVGRF_N_Neugebauer[i] <-  
-    exp(5.247 + (0.271 * hip_vert_df$pVACC_g[i]) + (0.014 * hip_vert_df$body_mass[i]))
+obese_data$pVGRF_N_Neugebauer <- NA
+for (i in 1:nrow(obese_data)) {
+  obese_data$pVGRF_N_Neugebauer[i] <-  
+    exp(5.247 + (0.271 * obese_data$pVACC_g[i]) + (0.014 * obese_data$body_mass[i]))
+}
+
+non_obese_data$pVGRF_N_Neugebauer <- NA
+for (i in 1:nrow(non_obese_data)) {
+  non_obese_data$pVGRF_N_Neugebauer[i] <-  
+    exp(5.247 + (0.271 * non_obese_data$pVACC_g[i]) + (0.014 * non_obese_data$body_mass[i]))
 }
 
 # Bland-Altman plots ------------------------------------------------------
 
-our_BA_plot  <- get_BA_plot(hip_vert_df, "pVGRF_N", "pVGRF_N_predicted")
+obese_our_BA_plot  <- get_BA_plot(obese_data, "pVGRF_N", "pVGRF_N_predicted")
 
-Neug_BA_plot <-  get_BA_plot(hip_vert_df, "pVGRF_N", "pVGRF_N_Neugebauer")
+obese_Neug_BA_plot <-  get_BA_plot(obese_data, "pVGRF_N", "pVGRF_N_Neugebauer")
+
+non_obese_our_BA_plot  <- get_BA_plot(non_obese_data, "pVGRF_N", "pVGRF_N_predicted")
+
+non_obese_Neug_BA_plot <-  get_BA_plot(non_obese_data, "pVGRF_N", "pVGRF_N_Neugebauer")
 
 # Indices of accuracy -----------------------------------------------------
 
-our_accuracy  <- accuracy_indices(hip_vert_df, "pVGRF_N", "pVGRF_N_predicted")
+obese_our_accuracy  <- accuracy_indices(obese_data, "pVGRF_N", "pVGRF_N_predicted")
 
-Neug_accuracy <- accuracy_indices(hip_vert_df, "pVGRF_N", "pVGRF_N_Neugebauer")
+obese_Neug_accuracy <- accuracy_indices(obese_data, "pVGRF_N", "pVGRF_N_Neugebauer")
+
+non_obese_our_accuracy  <- accuracy_indices(non_obese_data, "pVGRF_N", "pVGRF_N_predicted")
+
+non_obese_Neug_accuracy <- accuracy_indices(non_obese_data, "pVGRF_N", "pVGRF_N_Neugebauer")
