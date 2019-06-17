b340a5071b0b3bdfc81ce10f7fa4e548369a393c
Lucas Veras <lucasdsveras@gmail.com>
2019-06-17 11:46:24 +0100

    Add Regressions comparison section

--- analysis.R
+++ analysis.R
@@ -239,7 +239,7 @@ hip_vert_accuracy <- accuracy_indices(LOOCV_hip_vert_LMM, "pVGRF_N", "pVGRF_N_pr
 
 # 7. ANOVA ----------------------------------------------------------------
 # ** 7.1. Resultant GRF ---------------------------------------------------
-# **** 7.1.1. Building data frame -----------------------------------------
+# **** 7.1.1. Build data frame --------------------------------------------
 
 ## Predicted pRGRF
 ankle_res_pred <- LOOCV_ankle_res_LMM %>% 
@@ -400,7 +400,7 @@ res_ANOVA_s6 <- ezANOVA(
 )
 
 # ** 7.2. Vertical GRF ----------------------------------------------------
-# **** 7.2.1. Building data frame -----------------------------------------
+# **** 7.2.1. Build data frame --------------------------------------------
 
 ## Predicted pVGRF
 ankle_vert_pred <- LOOCV_ankle_vert_LMM %>% 
@@ -559,3 +559,113 @@ vert_ANOVA_s6 <- ezANOVA(
   detailed = TRUE,
   type     = 3
 )
+
+
+# 8. Regressions comparison -----------------------------------------------
+
+# ** 8.1 Prepare data frame -----------------------------------------------
+
+non_obese_df <- read_csv("~/Dropbox/Projects/walking_GRF_ACC/LOOCV_hip_vert.csv") %>% 
+  select(ID, speed, body_mass, BMI, BMI_cat, pVACC_g, pVGRF_N, pVGRF_N_predicted) %>% 
+  filter(BMI < 30)
+
+obese_df <- read_csv("~/Dropbox/Projects/walking_GRF_ACC/LOOCV_hip_vert.csv") %>% 
+  select(ID, speed, body_mass, BMI, BMI_cat, pVACC_g, pVGRF_N, pVGRF_N_predicted) %>% 
+  filter(BMI >= 30)
+
+whole_sample_df <- read_csv("~/Dropbox/Projects/walking_GRF_ACC/LOOCV_hip_vert.csv") %>% 
+  select(ID, speed, body_mass, BMI, BMI_cat, pVACC_g, pVGRF_N, pVGRF_N_predicted)
+
+# ** 8.2 Apply Neugebauer 2014 equation -----------------------------------
+
+non_obese_df$pVGRF_N_Neugebauer <- NA
+for (i in 1:nrow(non_obese_df)) {
+  non_obese_df$pVGRF_N_Neugebauer[i] <-  
+    exp(5.247 + (0.271 * non_obese_df$pVACC_g[i]) + (0.014 * non_obese_df$body_mass[i]))
+}
+
+obese_df$pVGRF_N_Neugebauer <- NA
+for (i in 1:nrow(obese_df)) {
+  obese_df$pVGRF_N_Neugebauer[i] <-  
+    exp(5.247 + (0.271 * obese_df$pVACC_g[i]) + (0.014 * obese_df$body_mass[i]))
+}
+
+whole_sample_df$pVGRF_N_Neugebauer <- NA
+for (i in 1:nrow(whole_sample_df)) {
+  whole_sample_df$pVGRF_N_Neugebauer[i] <-  
+    exp(5.247 + (0.271 * whole_sample_df$pVACC_g[i]) + (0.014 * whole_sample_df$body_mass[i]))
+}
+
+# ** 8.3 Bland-Altman plots -----------------------------------------------
+
+non_obese_our_BA_plot     <- get_BA_plot(non_obese_df, "pVGRF_N", "pVGRF_N_predicted")
+non_obese_Neug_BA_plot    <-  get_BA_plot(non_obese_df, "pVGRF_N", "pVGRF_N_Neugebauer")
+obese_our_BA_plot         <- get_BA_plot(obese_df, "pVGRF_N", "pVGRF_N_predicted")
+obese_Neug_BA_plot        <-  get_BA_plot(obese_df, "pVGRF_N", "pVGRF_N_Neugebauer")
+whole_sample_our_BA_plot  <- get_BA_plot(whole_sample_df, "pVGRF_N", "pVGRF_N_predicted")
+whole_sample_Neug_BA_plot <-  get_BA_plot(whole_sample_df, "pVGRF_N", "pVGRF_N_Neugebauer")
+
+# Check whether bias is statisticaly different than 0
+# For non-obese sample
+# Our equation
+non_obese_df$our_diff <- non_obese_df$pVGRF_N - non_obese_df$pVGRF_N_predicted
+non_obese_df$our_mean <- (non_obese_df$pVGRF_N + non_obese_df$pVGRF_N_predicted) / 2
+t.test(non_obese_df$our_diff, mu = 0)
+# Neugebauer's equation
+non_obese_df$Neug_diff <- non_obese_df$pVGRF_N - non_obese_df$pVGRF_N_Neugebauer
+non_obese_df$Neug_mean <- (non_obese_df$pVGRF_N + non_obese_df$pVGRF_N_Neugebauer) / 2
+t.test(non_obese_df$Neug_diff, mu = 0)
+
+# For obese sample
+# Our equation
+obese_df$our_diff <- obese_df$pVGRF_N - obese_df$pVGRF_N_predicted
+obese_df$our_mean <- (obese_df$pVGRF_N + obese_df$pVGRF_N_predicted) / 2
+t.test(obese_df$our_diff, mu = 0)
+# Neugebauer's equation
+obese_df$Neug_diff <- obese_df$pVGRF_N - obese_df$pVGRF_N_Neugebauer
+obese_df$Neug_mean <- (obese_df$pVGRF_N + obese_df$pVGRF_N_Neugebauer) / 2
+t.test(obese_df$Neug_diff, mu = 0)
+
+# For whole sample
+# Our equation
+whole_sample_df$our_diff <- whole_sample_df$pVGRF_N - whole_sample_df$pVGRF_N_predicted
+whole_sample_df$our_mean <- (whole_sample_df$pVGRF_N + whole_sample_df$pVGRF_N_predicted) / 2
+t.test(whole_sample_df$our_diff, mu = 0)
+# Neugebauer's equation
+whole_sample_df$Neug_diff <- whole_sample_df$pVGRF_N - whole_sample_df$pVGRF_N_Neugebauer
+whole_sample_df$Neug_mean <- (whole_sample_df$pVGRF_N + whole_sample_df$pVGRF_N_Neugebauer) / 2
+t.test(whole_sample_df$Neug_diff, mu = 0)
+
+### Linear regressions to identify proportional bias
+# For non-obese sample
+# Our equation
+non_obese_our_BA_plot_LR <- lm(our_diff ~ our_mean, data = non_obese_df)
+summary(non_obese_our_BA_plot_LR)
+# Neugebauer's equation
+non_obese_Neug_BA_plot_LR <- lm(Neug_diff ~ Neug_mean, data = non_obese_df)
+summary(non_obese_Neug_BA_plot_LR)
+
+# For obese sample
+# Our equation
+obese_our_BA_plot_LR <- lm(our_diff ~ our_mean, data = obese_df)
+summary(obese_our_BA_plot_LR)
+# Neugebauer's equation
+obese_Neug_BA_plot_LR <- lm(Neug_diff ~ Neug_mean, data = obese_df)
+summary(obese_Neug_BA_plot_LR)
+
+# For whole sample
+# Our equation
+whole_sample_our_BA_plot_LR <- lm(our_diff ~ our_mean, data = whole_sample_df)
+summary(whole_sample_our_BA_plot_LR)
+# Neugebauer's equation
+whole_sample_Neug_BA_plot_LR <- lm(Neug_diff ~ Neug_mean, data = whole_sample_df)
+summary(whole_sample_Neug_BA_plot_LR)
+
+# ** 8.4 Indices of accuracy ----------------------------------------------
+
+non_obese_our_accuracy     <- accuracy_indices(non_obese_df, "pVGRF_N", "pVGRF_N_predicted")
+non_obese_Neug_accuracy    <- accuracy_indices(non_obese_df, "pVGRF_N", "pVGRF_N_Neugebauer")
+obese_our_accuracy         <- accuracy_indices(obese_df, "pVGRF_N", "pVGRF_N_predicted")
+obese_Neug_accuracy        <- accuracy_indices(obese_df, "pVGRF_N", "pVGRF_N_Neugebauer")
+whole_sample_our_accuracy  <- accuracy_indices(whole_sample_df, "pVGRF_N", "pVGRF_N_predicted")
+whole_sample_Neug_accuracy <- accuracy_indices(whole_sample_df, "pVGRF_N", "pVGRF_N_Neugebauer")
