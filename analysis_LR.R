7dba53f407bf3863567b777776358f14db4514c6
Lucas Veras <lucasdsveras@gmail.com>
2019-12-17 16:26:16 +0000

    Add analysis of hip accelerometer data

--- analysis_LR.R
+++ analysis_LR.R
@@ -21,6 +21,13 @@ back_LR <- read_csv(here("data", "max_rates_back_IMU.csv")) %>%
     ID, speed, body_mass, height, BMI, BMI_cat, sex, age, pVLR_Ns, pVATR_gs, pRLR_Ns, pRATR_gs
   )
 
+hip_LR <- read_csv(here("data", "max_rates_hip_IMU.csv")) %>% 
+  left_join(anthopometric, by = c("ID", "speed")) %>% 
+  BMI_categories() %>% 
+  select(
+    ID, speed, body_mass, height, BMI, BMI_cat, sex, age, pVLR_Ns, pVATR_gs, pRLR_Ns, pRATR_gs
+  )
+
 # Explore -----------------------------------------------------------------
 
 ggplot(data = back_LR) +
@@ -46,9 +53,20 @@ back_res_LR_LMM <- lme(
   random = ~ 1 | ID,
   method = "ML",
   correlation = corAR1(),
-  data = back_LR
+  data = back_LR,
+  na.action = na.omit
 )
 rsquared(back_res_LR_LMM)
+
+hip_res_LR_LMM <- lme(
+  fixed = pRLR_Ns ~ pRATR_gs + I(pRATR_gs^2) + body_mass + pRATR_gs:body_mass,
+  random = ~ 1 | ID,
+  method = "ML",
+  correlation = corAR1(),
+  data = hip_LR,
+  na.action = na.omit
+)
+rsquared(hip_res_LR_LMM)
 
 # 3. Leave-one-out cross validation ---------------------------------------
 
@@ -57,15 +75,21 @@ fix_eff    <- pRLR_Ns ~ pRATR_gs + I(pRATR_gs^2) + body_mass + pRATR_gs:body_mas
 rand_eff   <- ~ 1 | ID
 # Back
 LOOCV_back_res_LR_LMM <- do.call(rbind, (lapply(unique(back_LR$ID), cross_validate_mixed_model, df = back_LR)))
+# Hip
+LOOCV_hip_res_LR_LMM <- do.call(rbind, (lapply(unique(hip_LR$ID), cross_validate_mixed_model, df = hip_LR)))
 
 # 4. Bland-Altman plots ---------------------------------------------------
 
 # For resultant peak loading rate
 # Back
 back_res_LR_BA_plot <- get_BA_plot(LOOCV_back_res_LR_LMM, "pRLR_Ns", "pRLR_Ns_predicted")
+# Back
+hip_res_LR_BA_plot <- get_BA_plot(LOOCV_hip_res_LR_LMM, "pRLR_Ns", "pRLR_Ns_predicted")
 
 # 5. Indices of accuracy --------------------------------------------------
 
 # For resultant peak loading rate
 # Back
 back_res_LR_accuracy <- accuracy_indices(LOOCV_back_res_LR_LMM, "pRLR_Ns", "pRLR_Ns_predicted")
+# Hip
+hip_res_LR_accuracy <- accuracy_indices(LOOCV_hip_res_LR_LMM, "pRLR_Ns", "pRLR_Ns_predicted")
